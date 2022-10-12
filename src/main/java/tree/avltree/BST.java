961c50a3a73b3330f7c570b7fc90549937d61286
tore <240870@usn.no>
2022-10-12 12:43:48 +0200

    c

--- src/main/java/tree/avltree/BST.java
+++ src/main/java/tree/avltree/BST.java
@@ -3,81 +3,269 @@ package tree.avltree;
 import java.util.Comparator;
 import java.util.Iterator;
 
-public class BST <E> implements Tree<E> {
+public class BST <E extends Comparable<E>> implements Tree<E> {
 
     protected TreeNode<E> root;
-    protected int size=0;
+    protected int size = 0;
     protected java.util.Comparator<E> c;
-    public BST(){
-        this.c= (e1, e2) ->((Comparable<E>)e1).compareTo(e2);
+
+    public BST() {
+        this.c = (e1, e2) -> ((Comparable<E>) e1).compareTo(e2);
     }
-    public BST(java.util.Comparator<E>c){
-        this.c=c;
+
+    public BST(java.util.Comparator<E> c) {
+        this.c = c;
     }
-    public BST(E[] objects){
-        this.c= (e1, e2) ->((Comparable<E>)e1).compareTo(e2);
-        for(int i=0; i< objects.length; i++){
+
+    public BST(E[] objects) {
+        this.c = (e1, e2) -> ((Comparable<E>) e1).compareTo(e2);
+        for (int i = 0; i < objects.length; i++) {
             add(objects[i]);
         }
     }
+
     @Override
     public boolean søk(E e) {
-    TreeNode<E> current = root;
+        TreeNode<E> current = root;
 
-    while(current!= null){
-        if(c.compare(e, current.element)<0){
-            current=current.right;
+        while (current != null) {
+            if (c.compare(e, current.element) < 0) {
+                current = current.right;
+            } else if (c.compare(e, current.element) > 0) {
+                current = current.right;
+            } else return true;
         }
-        else if(c.compare(e, current.element) >0){
-            current=current.right;
-        }
-        else return true;
-    }
-    return false;
+        return false;
     }
 
 
     @Override
     public boolean leggInn(E e) {
-    if(root==null){
-        root= new TreeNode(e);
-    }
-    else {
-        TreeNode<E> parent=null;
-        TreeNode<E> current=root;
-        while(current!=null){
-            if(c.compare(e, current.element)<0){
-                parent=current;
-                current=current.left;
+        if (root == null) {
+            root = new TreeNode(e);
+        } else {
+            TreeNode<E> parent = null;
+            TreeNode<E> current = root;
+            while (current != null) {
+                if (c.compare(e, current.element) < 0) {
+                    parent = current;
+                    current = current.left;
+                } else if (c.compare(e, current.element) > 0) {
+                    parent = current;
+                    current = current.right;
+                } else return false;
+                if (c.compare(e, parent.element) < 0)
+                    parent.left = new TreeNode<>(e);
+                else
+                    parent.right = new TreeNode<>(e);
             }
-            else if(c.compare(e, current.element)>0){
-                parent=current;
-                current=current.right;
-            }
-            else return false;
 
         }
-
-    }
+        size++;
+        return true;
     }
 
     @Override
-    public boolean slett(E e) {
-        return false;
+    public void inorder() {
+        inorder(root);
+    }
+
+    protected void inorder(TreeNode<E> root) {
+        if (root == null) return;
+        inorder(root.left);
+        System.out.println(root.element + " ");
+        inorder(root.right);
+    }
+
+    @Override
+    public void postOrder() {
+        postOrder(root);
+    }
+
+    protected void postOrder(TreeNode<E> root) {
+        if (root == null) return;
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.println(root.element + " ");
+    }
+
+    @Override
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    protected void preOrder(TreeNode<E> root) {
+        if (root == null) return;
+        System.out.println(root.element + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+
+
+
+    public static class TreeNode<E> {
+        protected E element;
+        protected TreeNode<E> left;
+        protected TreeNode<E> right;
+
+        public TreeNode(E e) {
+            element = e;
+        }
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    /**
+     * Returns the root of the tree
+     */
+    public TreeNode<E> getRoot() {
+        return root;
+    }
+
+    /**
+     * Returns a path from the root leading to the specified element
+     */
+    public java.util.ArrayList<TreeNode<E>> path(E e) {
+        java.util.ArrayList<TreeNode<E>> list =
+                new java.util.ArrayList<>();
+        TreeNode<E> current = root; // Start from the root
+
+        while (current != null) {
+            list.add(current); // Add the node to the list
+            if (e.compareTo(current.element) < 0) {
+                current = current.left;
+            } else if (e.compareTo(current.element) > 0) {
+                current = current.right;
+            } else
+                break;
+        }
+
+        return list; // Return an array list of nodes
     }
 
     @Override
-    public Iterator<E> iterator() {
-        return null;
+    /** Delete an element from the binary tree.
+     Return true if the element is deleted successfully
+     Return false if the element is not in the tree */
+    public boolean slett(E e) {
+        TreeNode<E> parent = null;
+        TreeNode<E> current = root;
+        while (current != null) {
+            if (e.compareTo(current.element) < 0) {
+                parent = current;
+                current = current.left;
+            } else if (e.compareTo(current.element) > 0) {
+                parent = current;
+                current = current.right;
+            } else
+                break; // Element is in the tree pointed at by current
+        }
+        if (current == null)
+            return false; // Element is not in the tree
+
+// Case 1: current has no left child
+        if (current.left == null) {
+// Connect the parent with the right child of the current node
+            if (parent == null) {
+                root = current.right;
+            }
+            else {
+                if (e.compareTo(parent.element) < 0)
+                    parent.left = current.right;
+                else
+                    parent.right = current.right;
+            }
+        }
+        else {
+// Case 2: The current node has a left child
+// Locate the rightmost node in the left subtree of
+// the current node and also its parent
+            TreeNode<E> parentOfRightMost = current;
+            TreeNode<E> rightMost = current.left;
+
+            while (rightMost.right != null) {
+                parentOfRightMost = rightMost;
+                rightMost = rightMost.right; // Keep going to the right
+            }
+
+// Replace the element in current by the element in rightMost
+            current.element = rightMost.element;
+
+             // Eliminate rightmost node
+            if (parentOfRightMost.right == rightMost)
+                parentOfRightMost.right = rightMost.left;
+            else
+// Special case: parentOfRightMost == current
+                parentOfRightMost.left = rightMost.left;
+        }
+
+        size--;
+        return true; // Element deleted successfully
+    }
+
+    @Override
+    public java.util.Iterator<E> iterator() {
+        return new InorderIterator();
+    }
+
+    // Inner class InorderIterator
+    private class InorderIterator implements java.util.Iterator<E> {
+        // Store the elements in a list
+        private java.util.ArrayList<E> list =
+                new java.util.ArrayList<>();
+        private int current = 0; // Point to the current element in list
+
+        public InorderIterator() {
+            inorder(); //
+        }
+        private void inorder() {
+            inorder(root);
+        }
+
+        /** Inorder traversal from a subtree */
+        private void inorder(TreeNode<E> root) {
+            if (root == null)return;
+            inorder(root.left);
+            list.add(root.element);
+            inorder(root.right);
+        }
+
+        @Override /** More elements for traversing? */
+        public boolean hasNext() {
+            if (current < list.size())
+                return true;
+
+            return false;
+        }
+
+        @Override /** Get the current element and move to the next */
+        public E next() {
+            return list.get(current++);
+        }
+
+        @Override /** Remove the current element */
+        public void remove() {
+            if (current == 0) // next() has not been called yet
+                throw new IllegalStateException();
+
+            slett(list.get(--current));
+            list.clear(); // Clear the list
+            inorder(); // Rebuild the list
+        }
     }
 
     @Override
     public void clear() {
-
+        root = null;
+        size = 0;
+    }
+    protected TreeNode<E> createNewNode(E e){
+        return new TreeNode<>(e);
     }
 }
+
+
